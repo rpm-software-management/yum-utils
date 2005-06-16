5e7b39040d69e4cd5cdcaef5e116269303d8ee38
Panu Matilainen <pmatilai@laiskiainen.org>
2005-06-16 21:15:04 +0000

    Add plugin for setting tsflags from the cli

---
+++ plugins/tsflags/tsflags.py
@@ -0,0 +1,13 @@
+requires_api_version = '2.1'
+
+def init_hook(conduit):
+    parser = conduit.getOptParser()
+    parser.add_option('--tsflags', dest='tsflags')
+
+def postreposetup_hook(conduit):
+    opts, args = conduit.getCmdLine()
+    conf = conduit.getConf()
+    if opts.tsflags:
+        flags = opts.tsflags.split(',')
+        print flags
+        conf.setConfigOption('tsflags', flags)
