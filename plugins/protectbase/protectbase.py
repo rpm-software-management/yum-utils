b24a86ec5a317368b6541198f47844a4d07d4c43
Menno Smits <menno@freshfoo.com>
2006-06-16 14:33:05 +0000

    Added GPL header, author contact note and docstrings.

--- plugins/protectbase/protectbase.py
+++ plugins/protectbase/protectbase.py
@@ -1,3 +1,19 @@
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Library General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+# by Menno Smits <menno@freshfoo.com>
+
 '''
 This plugin allows certain repositories to be protected. Packages in the
 protected repositories can't be overridden by packages in non-protected
@@ -17,10 +33,15 @@ requires_api_version = '2.4'
 plugin_type = (TYPE_CORE,)
 
 def config_hook(conduit):
+    '''Add options to Yum's configuration
+    '''
     config.YumConf.protect = config.BoolOption(False)
     config.RepoConf.protect = config.Inherit(config.YumConf.protect)
 
 def exclude_hook(conduit):
+    '''Exclude packages from non-protected repositories that may upgrade
+    packages from protected repositories.
+    '''
     cnt = 0
 
     allrepos = conduit.getRepos().listEnabled()
