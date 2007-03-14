986d01a510bfcb9df82ad1038b0e7f617befbd8d
Tim Lauridisen <tla@rasmil.dk>
2007-03-14 14:19:22 +0000

    Make the skip-broken a little smarter and the make the output better.

--- plugins/skip-broken/skip-broken.py
+++ plugins/skip-broken/skip-broken.py
@@ -50,20 +50,34 @@ class CheckDependency:
         saveTs = copy.copy(self.base.tsInfo)
         goodlist = []
         badlist = []
+        self.resetTs()
+        goodTs = copy.copy(self.base.tsInfo)
         for txmbr in saveTs:
+            # check if the member already has been marked as ok
+            if goodTs.exists(txmbr.po.pkgtup):
+                continue
             self.resetTs()
             po = txmbr.po
             state = txmbr.output_state
+            self.logger(2,"**** Checking for dep problems  : %s " % str(txmbr.po))
             self.base.tsInfo.add(txmbr)
             (rescode, restring) = self.base.resolveDeps()
             # Did the resolveDeps want ok ?
             if rescode == 2:
+                # transfer all member in this transaction to a good ts.
+                for good in self.base.tsInfo:
+                    if not goodTs.exists(good.po.pkgtup):
+                        self.logger(2,"****   OK : %s" % str(good))
+                        goodTs.add(good)
                 goodlist.append(txmbr)
             else:
+                self.logger(2,"****   Failed ")
+                for err in restring:
+                    self.logger(2,"****     %s " % err) 
                 badlist.append([txmbr,restring])
         # Restore self.tsInfo
         self.resetTs() 
-        self.base.tsInfo = saveTs
+        self.base.tsInfo = goodTs
         return goodlist,badlist
     
     def dumpTsInfo(self):
@@ -101,20 +115,23 @@ def preresolve_hook(conduit):
     if hasattr(opts,'skipbroken'):
         if opts.skipbroken or check_always:
             # get yum base
-            conduit.info(2,'Checking packages for dependency problems')
+            conduit.info(2,'**** Checking packages for dependency problems')
             base = conduit._base
             cd = CheckDependency(base,conduit.info)
             cd.dumpTsInfo()
             (good,bad) = cd.preDepCheck()
-            tsInfo = base.tsInfo
+            #tsInfo = base.tsInfo
+            conduit.info(2,"**** Packages with dependency resolving errors ")
+            
             for txmbr,err in bad:
                 # Removing bad packages for self.tsInfo
-                tsInfo.remove(txmbr.po.pkgtup)
-                conduit.info(2,"%s failed dependency resolving " % txmbr.po)
-                conduit.info(2,"%s " % err[0])
+                #tsInfo.remove(txmbr.po.pkgtup)
+                conduit.info(2,"**** %s " % txmbr.po)
+                for e in err:
+                    conduit.info(2,"****   %s " % 2)
             for txmbr in good:
-                conduit.info(3,"%s completed dependency resolving " % txmbr.po)    
+                conduit.info(3,"**** %s completed dependency resolving " % txmbr.po)    
             # Show the current state of y.tsInfo
-            conduit.info(2,'End Checking packages for dependency problems')
-            cd.dumpTsInfo()
+            conduit.info(2,'**** End Checking packages for dependency problems')
+            #cd.dumpTsInfo()
         
