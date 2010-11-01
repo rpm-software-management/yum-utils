66c66cd1fd978c2f012e23ce729ada471bff2b4c
James Antill <james@and.org>
2010-11-01 09:06:26 -0400

    Use arch as well as name in fmt_requires.

--- repoquery.py
+++ repoquery.py
@@ -905,7 +905,7 @@ class YumBaseQuery(yum.YumBase):
         for pkg in self.returnByName(name):
             for req in pkg.prco("requires"):
                 for res in self.fmt_whatprovides(req):
-                    pkgs[res.name] = res
+                    pkgs[(res.name, res.pkg.arch)] = res
         return pkgs.values()
 
     def fmt_location(self, name):
