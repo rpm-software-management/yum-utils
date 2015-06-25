1555cfa6465e6e31515a86f097c8993d89c0085e
Valentina Mukhamedzhanova <vmukhame@redhat.com>
2015-06-25 12:30:13 +0200

    ovl plugin: fix indentation

--- plugins/ovl/ovl.py
+++ plugins/ovl/ovl.py
@@ -25,24 +25,24 @@ base_dir = 'var/lib/rpm/'
 mtab = '/etc/mtab'
 
 def should_touch():
-        """ 
-        Touch the files only once we've verified that
-        we're on overlay mount
-        """
-        with open(mtab, 'r') as f:
-                line = f.readline()
-                return line and line.startswith('overlay /')
-        return False
+    """ 
+    Touch the files only once we've verified that
+    we're on overlay mount
+    """
+    with open(mtab, 'r') as f:
+        line = f.readline()
+        return line and line.startswith('overlay /')
+    return False
 
 def init_hook(conduit):
-        if not should_touch():
-                return
+    if not should_touch():
+        return
     ir = conduit.getConf().installroot
-        try:
-                for root, _, files in walk(path.join(ir, base_dir)):
-                        for f in files:
-                                p = path.join(root, f)
-                                with open(p, 'a'):
-                                        utime(p, None)
-        except Exception as e:
-                conduit.error(1, "Error while doing RPMdb copy-up:\n%s" % e)
+    try:
+        for root, _, files in walk(path.join(ir, base_dir)):
+            for f in files:
+                p = path.join(root, f)
+                with open(p, 'a'):
+                    utime(p, None)
+    except Exception as e:
+        conduit.error(1, "Error while doing RPMdb copy-up:\n%s" % e)
